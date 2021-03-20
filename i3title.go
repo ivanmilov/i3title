4b2ea912df8506d6f3e131ba60c206f416767c06
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-03-20 14:44:28 +0100

    check empty WS

--- i3title.go
+++ i3title.go
@@ -3,18 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	"go.i3wm.org/i3/v4"
 )
 
 func main() {
-
 	var truncate = flag.Int("t", 0, "truncate symbols")
 	flag.Parse()
 
 	t, _ := i3.GetTree()
-	if f := get_focused(t.Root); f != nil {
+	if f := get_focused_win(t.Root); f != nil {
 		print_win_title(f.WindowProperties.Title, *truncate)
 	}
 
@@ -22,11 +20,21 @@ func main() {
 	for recv.Next() {
 		switch e := recv.Event().(type) {
 		case *i3.WindowEvent:
-			title := e.Container.WindowProperties.Title
+			title := ""
+
+			if e.Change == "close" {
+				if t, _ := i3.GetTree(); t.Root != nil {
+					if f := get_focused_win(t.Root); f != nil {
+						title = f.WindowProperties.Title
+					}
+				}
+			} else {
+				title = e.Container.WindowProperties.Title
+			}
 			print_win_title(title, *truncate)
 		case *i3.WorkspaceEvent:
-			if get_focused(&e.Current) == nil {
-				if e.Change == "rename" || e.Change == "focus" {
+			if e.Change == "focus" {
+				if w := get_focused_win(&e.Current); w == nil {
 					print_win_title("", *truncate)
 				}
 			}
@@ -44,6 +52,6 @@ func print_win_title(title string, truncate int) {
 	fmt.Printf("%s\n", title)
 }
 
-func get_focused(n *i3.Node) *i3.Node {
-	return n.FindFocused(func(n *i3.Node) bool { return n.Type == i3.Con && len(n.Nodes) == 0 })
+func get_focused_win(n *i3.Node) *i3.Node {
+	return n.FindFocused(func(n *i3.Node) bool { return n.Focused && n.Type == "con" })
 }
